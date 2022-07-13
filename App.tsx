c79b00d8169d2dea03bdc65b527a22c418e63577
BoxPistols <ito.atsu.mail@gmail.com>
2022-07-14 04:02:00 +0900

    1 file modified

--- App.tsx
+++ App.tsx
@@ -2,14 +2,25 @@ import * as React from 'react';
 import './src/assets/style/style.scss';
 
 export default function App() {
+  const clickhandle = () => {
+    alert('Check');
+  };
+
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p className="fs-lg _bold">Start editing to see some magic happen</p>
 
-      <div className="size_md box" />
+      <div className="size_h-ss" />
+      <div className="size_md box" onClick={clickhandle} />
       <div className="size_h-ss" />
       <div className="size_xl box" />
+      <div className="size_h-ss" />
+      <div className="size_xl-5 box" />
     </div>
   );
+}
+
+function style(style: any) {
+  throw new Error('Function not implemented.');
 }
